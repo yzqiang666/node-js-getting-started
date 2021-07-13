eda3d2f372e9eb1ba692576e59906624c54724b0
yzqiang666 <yzqiang6@gmail.com>
2021-07-13 17:08:28 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-const express = require('express')
-const path = require('path')
 const PORT = process.env.PORT || 5000
+var http = require('http');
+ 
+var server = http.createServer(function (req, res) {
+    res.writeHead(301, {'Location': 'http://itbilu.com/'});
+    console.log(res._header);
+    res.end();
+});
+ 
+server.listen(PORT);
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
